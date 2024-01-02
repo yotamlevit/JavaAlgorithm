bbf780643d698f40409a13453b91b1bc67c215cc
yotamlevit <yotam.levit@gmail.com>
2024-01-02 09:17:54 +0200

    Adeed space line in docs

--- src/Ex12/Ex12.java
+++ src/Ex12/Ex12.java
@@ -47,7 +47,7 @@ public class Ex12 {
     /**
      *A recursive function that finds all the digits in a number that divides by a given digit
      * And returns it as a number
-     *
+     *<br><br>
      * For example: digit = 3, num = 387960 the number will find the numbers 3,9,6,0 and will return 3960
      * <br><br>
      * @param num: A number to find the sub number in.
