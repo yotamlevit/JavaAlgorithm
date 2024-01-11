444789b5c5bd00699e94808ef790d30b2d90e094
yotamlevit <yotam.levit@gmail.com>
2024-01-11 22:11:57 +0200

    Added max snake function

--- src/Ex13/Ex13.java
+++ src/Ex13/Ex13.java
@@ -39,4 +39,51 @@ public class Ex13 {
             return option1.length() > option2.length() ? option2 : option1;
         }
     }
+
+
+    public static boolean isBorder(int[][] mat, int i, int j) {
+        return i == -1 || j == -1 || mat.length == i || mat[0].length == j;
+    }
+
+    public static int maxSnake(int[][] mat, int i, int j, int prevVal, boolean[][] visited) {
+        int moveUp, moveDown, moveRight, moveLeft, maxMove;
+
+        // Brake condition - the indexes are border
+        if (isBorder(mat, i, j))
+            return 0;
+
+        // Brake condition - the difference between the cells values is less than 1
+        //                   or the cell is the origin
+        if (Math.abs(mat[i][j] - prevVal) != 1 || visited[i][j])
+            return 0;
+
+        //Brake condition - got to then bottom-right corner
+        if (mat.length - 1 == i && mat[0].length -1 == j)
+            return 1;
+
+        // Mark origin cell - to not loop back and forth of the same cells
+        visited[i][j] = true;
+
+        moveUp = maxSnake(mat, i-1, j, mat[i][j], visited);
+        moveDown = maxSnake(mat, i+1, j, mat[i][j], visited);
+        moveRight = maxSnake(mat, i, j+1, mat[i][j], visited);
+        moveLeft = maxSnake(mat, i, j-1, mat[i][j], visited);
+
+        // Remove mark from origin cell
+        visited[i][j] = false;
+
+        maxMove = Math.max(Math.max(moveLeft, moveRight), Math.max(moveUp, moveDown));
+
+        if (maxMove == 0)
+            return Integer.MIN_VALUE;
+
+        return 1 + maxMove;
+    }
+
+    public static int maxSnake(int[][] mat) {
+        // Initialize a boolean matrix to keep track of visited cells
+        boolean[][] visited = new boolean[mat.length][mat[0].length];
+
+        return maxSnake(mat, 0, 0, mat[0][0]+1, visited);
+    }
 }
--- src/Ex13/Ex13Tester.java
+++ src/Ex13/Ex13Tester.java
@@ -3,6 +3,19 @@ package Ex13;
 public class Ex13Tester {
 
     public static void main(String[] args) {
+        System.out.println("------------------ Start Ex1 ------------------------ ");
+
+        testMinimalSt();
+
+        System.out.println("------------------ Finish Ex1 ------------------------ \n");
+        System.out.println("------------------ Start Ex2 ------------------------ ");
+
+        testMaxSnake();
+
+        System.out.println("------------------ Finish Ex2 ------------------------ ");
+    }
+
+    private static void testMinimalSt() {
         testMinimalSt("AGGTAB", "GXTXAYB", "AGGXTXAYB");
         testMinimalSt("B", "A", "BA");
         testMinimalSt("AA", "A", "AA");
@@ -15,6 +28,40 @@ public class Ex13Tester {
         String result = Ex13.minimalSt(st1, st2);
         System.out.println("Test with st1 = \"" + st1 + "\", st2 = \"" + st2 + "\": " +
                 (result.equals(expected) ? "PASS" : "FAIL") +
+                " (Expected: \"" + expected + "\", Got: \"" + result + "\")");
+    }
+
+    private static void testMaxSnake() {
+        int[][] mat = {
+                {1,2,3,4,5,6},
+                {1,2,3,4,5,6},
+                {1,2,3,4,5,6},
+                {1,2,3,4,5,6},
+                {1,2,3,4,5,6},
+                {1,2,3,4,5,6}
+        };
+        testMaxSnake(mat, Integer.MIN_VALUE);
+
+        int[][] mat1 = {
+                {1,2},
+                {3,4},
+        };
+        testMaxSnake(mat1, Integer.MIN_VALUE);
+
+        int[][] mat2 = {
+                {4,5,2,3,1},
+                {3,4,1,4,4},
+                {1,5,6,7,8},
+                {3,4,5,8,9},
+                {3,2,2,7,6},
+        };
+        testMaxSnake(mat2, 13);
+    }
+
+    private static void testMaxSnake(int[][] mat, int expected) {
+        int result = Ex13.maxSnake(mat);
+        System.out.println("Test case " + mat + " " +
+                (result == expected ? "PASS" : "FAIL") +
                 " (Expected: \"" + expected + "\", Got: \"" + result + "\")");
     }
 }
