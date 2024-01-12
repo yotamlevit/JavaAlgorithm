e925d9f3b978042f192dc31a7a847cef59539c53
yotamlevit <yotam.levit@gmail.com>
2024-01-12 10:17:33 +0200

    Added documentation to rx13

--- src/Ex13/Ex13.java
+++ src/Ex13/Ex13.java
@@ -41,10 +41,47 @@ public class Ex13 {
     }
 
 
+    /**
+     *This function check if a given indexes are at the border of a matrix
+     <br><br>
+     *
+     * @param mat: The matrix
+     * @param i: The x (axis) index.
+     * @param j: The y (axis) index.
+     * @return boolean: Returns true if the indexes are border false if not
+     **/
     public static boolean isBorder(int[][] mat, int i, int j) {
         return i == -1 || j == -1 || mat.length == i || mat[0].length == j;
     }
 
+
+    /**
+     *A recursive function that calculates and explores possible snake path and returns
+     *  the length of the longest snake path.
+     <br><br>
+     *<br><br>
+     * A snake path is defined by:
+     * *<br><br>
+     *   1. the path starts on the first cell of the matrix (0,0)
+     *   *<br><br>
+     *   2. The last cell is the bottom right.
+     *   *<br><br>
+     *   3.The next cell can only be one from above, below, right, left. not diagnose
+     *   *<br><br>
+     *   4.The next cell can only be a cell where the difference of the values is max 1
+     *   *<br><br>
+     *   5. The math cannot return to the previous cell
+     *    *<br><br>
+     *<br><br>
+     *
+     * @param mat: The matrix
+     * @param i: The x (axis) index.
+     * @param j: The y (axis) index.
+     * @param prevVal: The previous value
+     * @param visited: A boolean matrix indicating which cells have been visited.
+     * @return int: Returns the maximum length of the snake-path from the current cell.
+     *              If no valid path is found the method returns Integer.MIN_VALUE.
+     **/
     public static int maxSnake(int[][] mat, int i, int j, int prevVal, boolean[][] visited) {
         int moveUp, moveDown, moveRight, moveLeft, maxMove;
 
@@ -54,14 +91,14 @@ public class Ex13 {
 
         // Brake condition - the difference between the cells values is less than 1
         //                   or the cell is the origin
-        if (Math.abs(mat[i][j] - prevVal) != 1 || visited[i][j])
+        if (Math.abs(mat[i][j] - prevVal) > 1 || visited[i][j])
             return 0;
 
         //Brake condition - got to then bottom-right corner
         if (mat.length - 1 == i && mat[0].length -1 == j)
             return 1;
 
-        // Mark origin cell - to not loop back and forth of the same cells
+        // Mark the current cell as visited- to not loop back and forth of the same cells
         visited[i][j] = true;
 
         moveUp = maxSnake(mat, i-1, j, mat[i][j], visited);
@@ -69,7 +106,7 @@ public class Ex13 {
         moveRight = maxSnake(mat, i, j+1, mat[i][j], visited);
         moveLeft = maxSnake(mat, i, j-1, mat[i][j], visited);
 
-        // Remove mark from origin cell
+        // Mark the current cell as unvisited before returning to allow other paths to use it
         visited[i][j] = false;
 
         maxMove = Math.max(Math.max(moveLeft, moveRight), Math.max(moveUp, moveDown));
@@ -80,6 +117,19 @@ public class Ex13 {
         return 1 + maxMove;
     }
 
+    /**
+     * This function calculates the maximum length of a "snake-path" in a given 2D matrix.
+     * A "snake-path" is defined as a path from the top left corner to the
+     * bottom right corner of the matrix, moving only to adjacent cells (not diagonally),
+     * where the difference in values between adjacent cells is at most 1.
+     * The path cannot visit a cell more than once. The method uses a recursive function to
+     * search and find the longest such path.
+     <br><br>
+     *
+     * @param mat: The 2D integer matrix in which to find the snake-path.
+     * @return The length of the longest snake-path. If no valid path is found,
+     *  *         the method returns Integer.MIN_VALUE.
+     **/
     public static int maxSnake(int[][] mat) {
         // Initialize a boolean matrix to keep track of visited cells
         boolean[][] visited = new boolean[mat.length][mat[0].length];
